02fb34e11bde1d6f5b761d599bc6b22a754098f8
hung.dang <danghung1202@gmail.com>
2017-02-13 09:58:08 +0700

    Add search box with animation

--- src/MyBrickset.WebApp/src/app/app.module.ts
+++ src/MyBrickset.WebApp/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { TdSearchBoxComponent } from './search/search-box/search-box.component';
+import { TdSearchInputComponent } from './search/search-input/search-input.component';
 import 'hammerjs';
 
 
 @NgModule({
     imports: [
         BrowserModule,
+        FormsModule,
         MaterialModule.forRoot()
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        TdSearchBoxComponent,
+        TdSearchInputComponent
     ],
     bootstrap: [ AppComponent ]
 })
--- src/MyBrickset.WebApp/src/main.ts
+++ src/MyBrickset.WebApp/src/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+
 import './assets/css/styles.css'; 
+import './assets/css/layout.scss';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
