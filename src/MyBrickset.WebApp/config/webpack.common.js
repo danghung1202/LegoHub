51ade037f4580d229adbf01b05324d38bc82d772
hung.dang <danghung1202@gmail.com>
2017-03-22 16:29:18 +0700

    Add webpack config for production environment

--- src/MyBrickset.WebApp/config/webpack.common.js
+++ src/MyBrickset.WebApp/config/webpack.common.js
@@ -1,9 +1,16 @@
 
-var webpack = require('webpack');
+const webpack = require('webpack');
 const helpers = require('./helpers');
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
+const NormalModuleReplacementPlugin = require('webpack/lib/NormalModuleReplacementPlugin');
+
 
 module.exports = {
     entry: {
@@ -30,9 +37,14 @@ module.exports = {
                 include: [helpers.root('src', 'app')]
             },
             {
+                test: /\.json$/,
+                use: 'json-loader'
+            },
+            {
                 test: /\.css$/,
                 loaders: ['to-string-loader', 'css-loader'],
-                include: [helpers.root('src', 'app')]
+                include: [helpers.root('src', 'app')],
+                exclude: [helpers.root('src', 'styles')]
             },
             {
                 test: /\.css$/,
@@ -40,12 +52,15 @@ module.exports = {
                     fallbackLoader: 'style-loader',
                     loader: 'css-loader'
                 }),
-                exclude: [helpers.root('src', 'app')]
+                exclude: [helpers.root('src', 'app')],
+                include: [helpers.root('src', 'styles')]
             },
+
             {
                 test: /\.(scss|sass)$/,
                 loaders: ['to-string-loader', 'css-loader', 'sass-loader'],
-                include: [helpers.root('src', 'app')]
+                include: [helpers.root('src', 'app')],
+                exclude: [helpers.root('src', 'styles')]
             },
             {
                 test: /\.(scss|sass)$/,
@@ -53,8 +68,10 @@ module.exports = {
                     fallbackLoader: 'style-loader',
                     loader: 'css-loader!sass-loader'
                 }),
-                exclude: [helpers.root('src', 'app')]
+                exclude: [helpers.root('src', 'app')],
+                include: [helpers.root('src', 'styles')]
             },
+            
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
                 loader: 'file-loader'
@@ -63,9 +80,77 @@ module.exports = {
     },
 
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
+
+        /*
+       * Plugin: ForkCheckerPlugin
+       * Description: Do type checking in a separate process, so webpack don't need to wait.
+       *
+       * See: https://github.com/s-panferov/awesome-typescript-loader#forkchecker-boolean-defaultfalse
+       */
+        new CheckerPlugin(),
+
+        new CommonsChunkPlugin({
             name: ['app', 'vendor', 'polyfills']
-        })
+        }),
+
+        /**
+       * Plugin: ContextReplacementPlugin
+       * Description: Provides context to Angular's use of System.import
+       *
+       * See: https://webpack.github.io/docs/list-of-plugins.html#contextreplacementplugin
+       * See: https://github.com/angular/angular/issues/11580
+       */
+        new ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+            helpers.root('src'), // location of your src
+            {
+                // your Angular Async Route paths relative to this root directory
+            }
+        ),
+
+        /*
+       * Plugin: CopyWebpackPlugin
+       * Description: Copy files and directories in webpack.
+       *
+       * Copies project static assets.
+       *
+       * See: https://www.npmjs.com/package/copy-webpack-plugin
+       */
+        new CopyWebpackPlugin([
+            { from: 'src/assets', to: 'assets' },
+            { from: 'src/meta' }
+        ]),
+
+        /**
+       * Plugin LoaderOptionsPlugin (experimental)
+       *
+       * See: https://gist.github.com/sokra/27b24881210b56bbaff7
+       */
+        new LoaderOptionsPlugin({}),
+
+        // Fix Angular 2
+        new NormalModuleReplacementPlugin(
+            /facade(\\|\/)async/,
+            helpers.root('node_modules/@angular/core/src/facade/async.js')
+        ),
+        new NormalModuleReplacementPlugin(
+            /facade(\\|\/)collection/,
+            helpers.root('node_modules/@angular/core/src/facade/collection.js')
+        ),
+        new NormalModuleReplacementPlugin(
+            /facade(\\|\/)errors/,
+            helpers.root('node_modules/@angular/core/src/facade/errors.js')
+        ),
+        new NormalModuleReplacementPlugin(
+            /facade(\\|\/)lang/,
+            helpers.root('node_modules/@angular/core/src/facade/lang.js')
+        ),
+        new NormalModuleReplacementPlugin(
+            /facade(\\|\/)math/,
+            helpers.root('node_modules/@angular/core/src/facade/math.js')
+        ),
+
         // ,
         // new HtmlWebpackPlugin({
         //     template: 'index.html',
@@ -74,5 +159,21 @@ module.exports = {
         //     //metadata: METADATA,
         //     inject: 'head'
         // }),
-    ]
-};
+
+
+    ],
+    /*
+     * Include polyfills or mocks for various node stuff
+     * Description: Node configuration
+     *
+     * See: https://webpack.github.io/docs/configuration.html#node
+     */
+    node: {
+        global: true,
+        crypto: 'empty',
+        process: true,
+        module: false,
+        clearImmediate: false,
+        setImmediate: false
+    }
+}
--- src/MyBrickset.WebApp/config/webpack.dev.js
+++ src/MyBrickset.WebApp/config/webpack.dev.js
@@ -1,12 +1,19 @@
-var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var commonConfig = require('./webpack.common.js');
+const helpers = require('./helpers');
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
 
-const path = require('path');
-const rootDir = path.resolve(__dirname, '..');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 
-console.log(__dirname);
-console.log(rootDir);
+
+/**
+ * Webpack Constants
+ */
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+const METADATA =  {
+  ENV: ENV
+};
 
 module.exports = webpackMerge(commonConfig, {
 
@@ -14,19 +21,90 @@ module.exports = webpackMerge(commonConfig, {
     devtool: 'source-map',
 
     output: {
-        //path: path.resolve(rootDir, 'MyBrickset.WebApi/wwwroot/scripts'),
-        path: './dist',
-        //publicPath: 'http://brickset.dev/',
-        filename: '[name].js'
-        //chunkFilename: '[id].chunk.js'
+        /**
+       * The output directory as absolute path (required).
+       *
+       * See: http://webpack.github.io/docs/configuration.html#output-path
+       */
+        path: helpers.root('dist'),
+
+        /**
+       * Specifies the name of each output file on disk.
+       * IMPORTANT: You must not specify an absolute path here!
+       *
+       * See: http://webpack.github.io/docs/configuration.html#output-filename
+       */
+        filename: '[name].js',
+
+        /**
+       * The filename of the SourceMaps for the JavaScript files.
+       * They are inside the output.path directory.
+       *
+       * See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
+       */
+        sourceMapFilename: '[file].map',
+
+        /** The filename of non-entry chunks as relative path
+         * inside the output.path directory.
+         *
+         * See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
+         */
+        chunkFilename: '[id].chunk.js',
     },
 
     plugins: [
-        new ExtractTextPlugin('[name].css')
-    ]
 
-    // devServer: {
-    //     historyApiFallback: true,
-    //     stats: 'minimal'
-    // }
+        /**
+       * Plugin: ExtractTextPlugin
+       * Description: Extracts imported CSS files into external stylesheet
+       *
+       * See: https://github.com/webpack/extract-text-webpack-plugin
+       */
+        new ExtractTextPlugin('[name].css'),
+
+        /**
+       * Plugin: DefinePlugin
+       * Description: Define free variables.
+       * Useful for having development builds with debug logging or adding global constants.
+       *
+       * Environment helpers
+       *
+       * See: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+       */
+        // NOTE: when adding more properties, make sure you include them in custom-typings.d.ts
+        new DefinePlugin({
+            'ENV': JSON.stringify(METADATA.ENV),
+            'process.env': {
+                'ENV': JSON.stringify(METADATA.ENV),
+                'NODE_ENV': JSON.stringify(METADATA.ENV),
+            }
+        }),
+
+        /**
+         * Plugin LoaderOptionsPlugin (experimental)
+         *
+         * See: https://gist.github.com/sokra/27b24881210b56bbaff7
+         */
+        new LoaderOptionsPlugin({
+            debug: true,
+            options: {
+
+            }
+        }),
+    ],
+
+    /*
+     * Include polyfills or mocks for various node stuff
+     * Description: Node configuration
+     *
+     * See: https://webpack.github.io/docs/configuration.html#node
+     */
+    node: {
+        global: true,
+        crypto: 'empty',
+        process: true,
+        module: false,
+        clearImmediate: false,
+        setImmediate: false
+    }
 });
