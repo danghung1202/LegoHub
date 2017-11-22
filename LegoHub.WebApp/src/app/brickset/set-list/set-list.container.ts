d2dfe1d34369bf630e960cc21f41d787e01ee019
NITECO\hung.dang <danghung1202@gmail.com>
2017-11-22 15:24:57 +0700

    Refactor Set List component using Container Component

--- LegoHub.WebApp/src/app/brickset/set-list/set-list.container.ts
+++ LegoHub.WebApp/src/app/brickset/set-list/set-list.container.ts
@@ -13,35 +13,23 @@ import { AppState, SetListActions, FilterActions, NavigationActions, ProgressBar
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `<set-list 
-                [criterias]="criterias | async" 
-                [loading]="loading | async" 
-                [criteriaName]="criteriaName" 
-                (goBack)="goBack()"
-                (applyClick)="applyCriterias($event)">
-            </set-list>`
+                    [sets]="sets | async" 
+                    [loading]="loading | async" 
+                    [showMore]="showMore | async" 
+                    [queryParams]="queryParams | async" 
+                    (openFilterPanelEvent)="openFilterPanel()"
+                    (openSortPanelEvent)="openSortPanel()"
+                    (loadMoreEvent)="loadMore($event)">
+                </set-list>`
 })
 export class SetListComponent implements OnInit {
-    subParams: Subscription;
-
+    queryParams: Observable<any>;
     sets: Observable<Set[]>;
-
     loading: Observable<boolean>;
     showMore: Observable<boolean>;
 
-    selectedThemes: Theme[];
-    selectedSubthemes: Subtheme[];
-    selectedYears: Year[];
-
-    params: any = {
-        themes: '',
-        subthemes: '',
-        years: ''
-    };
-
-    pageNumber: number = 1;
-
     constructor(
-        private route: ActivatedRoute,
+        public route: ActivatedRoute,
         private router: Router,
         private store: Store<AppState>,
         private setActions: SetListActions,
@@ -52,31 +40,10 @@ export class SetListComponent implements OnInit {
         this.sets = this.store.select(s => s.sets).select(s => s.sets);
         this.loading = this.store.select(s => s.sets).select(s => s.loading);
         this.showMore = this.store.select(s => !s.progress.isShow && s.sets.showMore);
-    }
-
-    ngOnInit() {
-        this.subParams = this.route
-            .queryParams
-            .subscribe(params => {
-                this.pageNumber = 1;
-                this.params = params;
-                let themes = params['themes'] || '';
-                let subthemes = params['subthemes'] || '';
-                let years = params['years'] || '';
-
-                this.selectedThemes = themes.split(',').filter(item => item.trim() != '').map((theme) => ({ theme: theme, isSelected: true }));
-
-                this.selectedSubthemes = subthemes.split(',').filter(item => item.trim() != '').map(subtheme => ({ subtheme: subtheme, isSelected: true }));
-
-                this.selectedYears = years.split(',').filter(item => item.trim() != '').map(year => ({ year: year, isSelected: true }));
-
-                this.store.dispatch(this.setActions.loadSets(themes, subthemes, years));
-            });
-
+        this.queryParams = this.route.queryParams;
     }
 
     ngOnDestroy() {
-        this.subParams.unsubscribe();
         this.store.dispatch(this.progressActions.showProgressBar(0));
     }
 
@@ -98,11 +65,7 @@ export class SetListComponent implements OnInit {
         return item ? item.setID : undefined;
     }
 
-    loadMore() {
-        let themes = this.params['themes'];
-        let subthemes = this.params['subthemes'];
-        let years = this.params['years'];
-        this.pageNumber = this.pageNumber + 1;
-        this.store.dispatch(this.setActions.loadMoreSets(themes, subthemes, years, '', this.pageNumber.toString()));
+    loadMore(param) {
+        this.store.dispatch(this.setActions.loadMoreSets(param.themes, param.subthemes, param.years, '', param.pageNumber.toString()));
     }
 }
